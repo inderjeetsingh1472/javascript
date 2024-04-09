d30b433717dd16ed959f4d974829884ea14effc5
inderjeet1472 <gate37814@gmail.com>
2024-04-09 19:40:51 +0530

    string

---
+++ string/script.js
@@ -0,0 +1,20 @@
+// let inder =(a,b)=>{
+//     return a+b
+// }
+// console.log(
+// inder(5,3)
+
+// )
+// let a='    inderjeet     singh     dandiwal  '
+// console.log(a)
+// console.log(a.length)
+// console.log(a.toUpperCase())
+// console.log(a.toLocaleLowerCase())
+// console.log(a.slice(1,5))
+// console.log(a.slice(10,15))
+// console.log(a.slice(5))
+// console.log(a.replace("dandiwal","singh"))
+// let newname=a.replace("dandiwal",' ')
+// console.log(newname)
+// console.log(a.concat('\n',newname,"dandiwal"))
+// console.log(a.trim())
---
+++ string/string.js
@@ -0,0 +1,1 @@
+let a='inderjeet singh dandiwal'
